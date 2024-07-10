ca7de1ccca759beede6eed0514f80ca4eb4dc132
Alexandra git@azrose.dev <git@azrose.dev>
2024-07-10 19:29:24 -0400

    add solution for 605

---
+++ 605/605-can-place-flowers.rs
@@ -0,0 +1,30 @@
+impl Solution {
+    pub fn can_place_flowers(mut flowerbed: Vec<i32>, n: i32) -> bool {
+        let mut plantable_count = 0;
+        let mut plantable = false;
+
+        let mut i = 0;
+
+        loop {
+            let window = (flowerbed.get(i-1), flowerbed.get(i), flowerbed.get(i+1));
+
+            plantable = match window {
+                (Some(prev), Some(curr), Some(next)) => *prev == 0 && *curr == 0 && *next == 0,
+                (Some(prev), Some(curr), None) =>  *prev == 0 && *curr == 0,
+                (None, Some(curr), Some(next)) => *curr == 0 && *next == 0,
+                (None, Some(curr), None) => *curr == 0,
+                _ => break,
+            };
+
+            if plantable {
+                i += 1;
+                plantable_count += 1;
+                plantable = false;
+            }
+
+            i += 1;
+        }
+
+        return plantable_count >= n;
+    }
+}
