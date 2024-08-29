7e62d159037696860b9018b7cca8f5fa02c11da3
Alexandra git@azrose.dev <git@azrose.dev>
2024-08-28 20:32:21 -0400

    add challenge problem: find unique value in sorted array

---
+++ challenges/find-unduplicated-in-sorted-array/README.md
@@ -0,0 +1,23 @@
+# Challenge A
+
+Given a **sorted** integer array of size `n`,
+where each value is either duplicated once or unique,
+find the unique value and return it.
+
+The solution should be an optimized solution.
+
+## Examples
+
+**Input:**
+`[-1, -1, 3, 3, 4, 5, 5, 9, 9]`
+
+**Output:**
+`4`
+
+---
+
+**Input:**
+`[-6, -5, -5, -2, -2, 14, 14, 20, 20, 21, 21, 82, 82, 1000, 1000, 1450, 1450]`
+
+**Output:**
+`-6`
---
+++ challenges/find-unduplicated-in-sorted-array/src/lib.rs
@@ -0,0 +1,70 @@
+pub fn find_unique_value(input: &[i64]) -> i64 {
+    return loop {
+        let bisected = split(input);
+
+        println!("{:?}", bisected);
+
+        let left_len = bisected[0].len();
+        let right_len = bisected[1].len();
+
+        // check the base cases
+
+        if left_len == 1 { // base case: return element on left
+            break bisected[0][0];
+        }
+
+        if right_len == 1 { // base case: return element on right
+            break bisected[1][0];
+        }
+
+        // choose which direction to continue with
+        // under ordinary conditions, we will use the direction with an odd number of elements for our next recursive iteration
+
+        let mut recurse_on = match (left_len % 2 != 0, right_len % 2 != 0) {
+            (true, false) => 0,
+            (false, true) => 1,
+            _ => 0,
+        };
+
+        // recurse
+        break find_unique_value(bisected[recurse_on]);
+    };
+}
+
+fn concat_slices(base: &[i64], add: i64) -> Vec<i64> {
+    let mut base_vec = base.to_vec();
+
+    base_vec.append(&mut vec![add]);
+
+    return base_vec;
+}
+
+fn split(input: &[i64]) -> [&[i64]; 2] {
+    let mut midpoint_index = input.len() / 2;
+
+    if input[midpoint_index - 1] == input[midpoint_index] {
+        midpoint_index -= 1;
+    }
+
+    let left = &input[0..midpoint_index];
+    let right = &input[midpoint_index..];
+
+    return [left, right];
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_find_unique_1() {
+        let input = [-1, -1, 3, 3, 4, 5, 5, 9, 9];
+        assert_eq!(find_unique_value(&input), 4);
+    }
+
+    #[test]
+    fn test_find_unique_2() {
+        let input = [-6, -5, -5, -2, -2, 14, 14, 20, 20, 21, 21, 82, 82, 1000, 1000, 1450, 1450];
+        assert_eq!(find_unique_value(&input), -6);
+    }
+}
